a8707058634cab4df827c015c66c1ac727d18fdb
quocnghia32 <nguyennghia3224@gmail.com>
2025-01-02 17:52:42 +0700

    Done Home Page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import AboutUs from "./AboutUs";
-import FeaturedProducts from "./FeaturedProducts";
-import LatestProducts from "./LatestProducts";
-import TrendingProducts from "./TrendingProducts";
+import ProductSlider from "./ProductsSlider";
+import products_featured from "../mock/featured_products.json";
+import products_latest from "../mock/latest_products.json";
+import products_trending from "../mock/trending_products.json";
 
 function Home() {
   return (
     <>
         <AboutUs />
-        <FeaturedProducts />
-        <LatestProducts />
-        <TrendingProducts />
+        <ProductSlider title = 'Featured Products' products={products_featured}/>
+        <ProductSlider title = 'Trending Products' products={products_latest}/>
+        <ProductSlider title = 'Latest Products' products={products_trending}/>
     </>
     
     
