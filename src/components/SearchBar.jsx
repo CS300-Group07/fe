0ee77945beada54b792edf77de6dac39ba160af3
quocnghia32 <nguyennghia3224@gmail.com>
2025-01-03 03:15:31 +0700

    Handle Enter Button

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,7 +6,8 @@ import { useNavigate } from 'react-router-dom';
 const SearchBar = ({ setProducts, setLoading, isIncreasingPrice}) => {
     const navigate = useNavigate();
     const [inputValue, setInputValue] = useState('');
-    const handleSearch = async () => {
+    const handleSearch = async (e) => {
+        e.preventDefault();
         if (inputValue.trim() !== '') {
         try {
             const num = 20;
@@ -33,21 +34,24 @@ const SearchBar = ({ setProducts, setLoading, isIncreasingPrice}) => {
     };
 
     return (
-        <div className="flex justify-center items-center mt-5">
-        <input
-            type="text"
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
-            placeholder="Search..."
-            className="w-full max-w-md px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent"
-        />
-        <button
-            onClick={handleSearch}
-            className="ml-2 px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
-        >
-            Search
-        </button>
-        </div>
+        <form onSubmit={handleSearch}>
+            <div className="flex justify-center items-center mt-5">
+                <input
+                type="text"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                placeholder="Search..."
+                className="w-full max-w-md px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent"
+                />
+                <button
+                    onClick={handleSearch}
+                    className="ml-2 px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+                >
+                Search
+                </button>
+            </div>
+        </form>
+        
     );
 };
 export default SearchBar;
