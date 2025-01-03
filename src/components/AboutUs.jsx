f6967e42f826e0b07df3e1743d49cc04718bf3f7
minhnguyen28204 <125328581+MinhNguyen282@users.noreply.github.com>
2025-01-03 23:12:49 +0700

    update about us

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -5,7 +5,7 @@ function AboutUs() {
     <section className="bg-gray-100 py-20">
       <div className="container mx-auto text-center">
         <h2 className="text-4xl font-bold mb-4">About Us</h2>
-        <p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <p className="text-gray-700">This is CS300 - Group 07 project!</p>
       </div>
     </section>
   );
